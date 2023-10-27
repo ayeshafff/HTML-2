20e58067454973c3accaf2d99bc13ecf5f759c5b
ayeshafff <136786241+ayeshafff@users.noreply.github.com>
2023-10-27 21:08:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('surveyForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Add desired actions on form submission, e.g. sending the form data to a server
+    alert('Form submitted!'); 
+});
